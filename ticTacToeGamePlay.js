364b7c7acb0cfababbade7025abfc3fd249d5caf
saraheliz7 <woodsose@gmail.com>
2017-11-27 18:23:20 -0500

    Grouped board functions and state into board object

--- ticTacToeGamePlay.js
+++ ticTacToeGamePlay.js
@@ -1,12 +1,66 @@
-var board = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""]
-];
-
 var count = 0;
 
 var gameOver = false;
+
+var Board = function(){
+  var grid = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ];
+
+  this.getGrid = function(){
+    return grid;
+  };
+
+  var hasHorizontalWin = function(player) {
+    for(var row = 0; row < grid.length; row++) {
+      if(grid[row][0] === player && grid[row][1] === player && grid[row][2] === player){
+        return true;
+      }
+    }
+  };
+
+  var hasVerticalWin = function(player) {
+    for(var column = 0; column < grid.length; column++) {
+      if(grid[0][column] === player && grid[1][column] === player && grid[2][column] === player){
+        return true;
+      }
+    }
+  };
+
+  var hasDiagonalWin = function(player) {
+    if(grid[0][0] === player && grid[1][1] === player && grid[2][2] === player) {
+      return true;
+    } else if (grid[0][2] === player && grid[1][1] === player && grid[2][0] === player) {
+      return true;
+    }
+  };
+
+  var hasWin = function(player){
+    return hasHorizontalWin(player) || hasVerticalWin(player) || hasDiagonalWin(player);
+  };
+
+  this.winner = function() {
+    if(hasWin("X")) {
+      return "X";
+    } else if(hasWin("O")) {
+      return "O";
+    } else {
+      return false;
+    }
+  };
+
+  this.setCellValue = function(row, column) {
+    if(count % 2 === 0) {
+      grid[row][column] = "O"
+    } else {
+      grid[row][column] = "X";
+    }
+    count++
+  };
+};
+
 
 var showCellValue = function(row, column, value) {
   var cell = document.querySelector(".cell-" + row + "-" + column);
@@ -18,80 +72,17 @@ var showCellValue = function(row, column, value) {
 };
 
 var drawBoard = function(board) {
-  for(var row = 0; row < board.length; row++) {
-    for(var column = 0; column < board[row].length; column++) {
-      showCellValue(row, column, board[row][column]);
+  for(var row = 0; row < board.getGrid().length; row++) {
+    for(var column = 0; column < board.getGrid()[row].length; column++) {
+      showCellValue(row, column, board.getGrid()[row][column]);
     }
   }
-};
-
-var hasHorizontalWin = function(board, player) {
-  for(var row = 0; row < board.length; row++) {
-    if(board[row][0] === player && board[row][1] === player && board[row][2] === player){
-      return true;
-    }
-  }
-};
-
-var hasVerticalWin = function(board, player) {
-  for(var column = 0; column < board.length; column++) {
-    if(board[0][column] === player && board[1][column] === player && board[2][column] === player){
-      return true;
-    }
-  }
-};
-
-var hasDiagonalWin = function(board, player) {
-  if(board[0][0] === player && board[1][1] === player && board[2][2] === player) {
-    return true;
-  } else if (board[0][2] === player && board[1][1] === player && board[2][0] === player) {
-    return true;
-  }
-};
-
-var hasWin = function(board, player){
-  return hasHorizontalWin(board, player) || hasVerticalWin(board, player) || hasDiagonalWin(board, player);
 };
 
 var showWinner = function(player){
   var winnerMessage = document.querySelector(".winner-message");
   winnerMessage.textContent = player + " WINS!";
   winnerMessage.classList.remove("hidden");
-};
-
-var winner = function(board) {
-  if(hasWin(board, "X")) {
-    return "X";
-  } else if(hasWin(board, "O")) {
-    return "O";
-  } else {
-    return false;
-  }
-};
-
-var setCellValue = function(row, column, board) {
-  if(count % 2 === 0) {
-    board[row][column] = "O"
-  } else {
-    board[row][column] = "X";
-  }
-  count++
-};
-
-var checkForWinner = function(board) {
-  var player = winner(board);
-  if(player) {
-    showWinner(player);
-    gameOver = true;
-  }
-};
-
-var makeMove = function(row, column, board) {
-  if(gameOver === false && board[row][column] === "") {
-    setCellValue(row, column, board);
-    drawBoard(board);
-    checkForWinner(board);
-  }
 };
 
 var attachCellClickHandler = function(row, column, board, callback) {
@@ -102,12 +93,35 @@ var attachCellClickHandler = function(row, column, board, callback) {
 };
 
 var attachBoardClickHandlers = function(board, callback) {
-  for(var row = 0; row < board.length; row++) {
-    for(var column = 0; column < board[row].length; column++) {
+  for(var row = 0; row < board.getGrid().length; row++) {
+    for(var column = 0; column < board.getGrid()[row].length; column++) {
       attachCellClickHandler(row, column, board, callback);
     }
   }
 };
+
+
+
+var checkForWinner = function(board) {
+  var player = board.winner();
+  if(player) {
+    showWinner(player);
+    gameOver = true;
+  }
+};
+
+
+
+
+var makeMove = function(row, column, board) {
+  if(gameOver === false && board.getGrid()[row][column] === "") {
+    board.setCellValue(row, column);
+    drawBoard(board);
+    checkForWinner(board);
+  }
+};
+
+var board = new Board();
 
 window.onload = function() {
   attachBoardClickHandlers(board, makeMove);
